89e98d69c3a3924994eeb2a8eb89231c133ddd9e
MuhammadHassanBhatti <mhassaniqbalbhatti@gmail.com>
2024-05-21 21:44:28 +0500

    chnages

--- src/components/TextUtilsForm.js
+++ src/components/TextUtilsForm.js
@@ -1,68 +1,71 @@
 import React from "react";
 import { useState } from "react";
 
-
 export default function TextUtilsForm(props) {
   const [text, setText] = useState("");
 
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
-    props.showAlert("Converted to UpperCase Succesfully" , "success")
+    props.showAlert("Converted to UpperCase Succesfully", "success");
   };
 
   const handlelowClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
-    props.showAlert("Converted to LowerCase Succesfully" , "success")
+    props.showAlert("Converted to LowerCase Succesfully", "success");
   };
 
-
   const handleCapClick = () => {
-    let newText =text.split(" ")
+    let newText = text.split(" ");
     const capitalizedText = newText.map((element) => {
-        return element.charAt(0).toUpperCase() + element.slice(1);
+      return element.charAt(0).toUpperCase() + element.slice(1);
     });
-    setText(capitalizedText.join(" ")); 
-    props.showAlert("Converted to Title Case Succesfully" , "success")
-};
+    setText(capitalizedText.join(" "));
+    props.showAlert("Converted to Title Case Succesfully", "success");
+  };
 
+  const handleCopy = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Text Copy to Clipboard Succesfully", "success");
+  };
 
-const handleCopy =() =>{
-  let text = document.getElementById("myBox")
-  text.select();
-  navigator.clipboard.writeText(text.value);
-  props.showAlert("Text Copy to Clipboard Succesfully" , "success")
-}
-
-const removeExtraSpaces =()=>{
-  let newText=text.split(/[ ]+/);
-  setText(newText.join(" "));
-  props.showAlert("Remove extra spaces Succesfully" , "success")
-}
+  const removeExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Remove extra spaces Succesfully", "success");
+  };
 
   const handleClearClick = () => {
     let newText = "";
     setText(newText);
-    props.showAlert("Clear text Succesfully" , "success")
+    props.showAlert("Clear text Succesfully", "success");
   };
 
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
 
-  const words = text.split(/\s+/).filter(word => word !== "");
+  const words = text.split(/\s+/).filter((word) => word !== "");
   const numberOfWords = words.length;
 
   return (
     <>
-      <div className="container my-4 mb-1" style={{color : props.mode==='light'?'black':'white'}}>
+      <div
+        className="container my-4 mb-1"
+        style={{ color: props.mode === "light" ? "black" : "white" }}
+      >
         <h3>Enter your text here :</h3>
         <textarea
           className="form-control"
           value={text}
           onChange={handleOnChange}
-          style={{backgroundColor : props.mode==='light'?'white':'grey' , color : props.mode==='light'?'black':'white'}}
+          style={{
+            backgroundColor: props.mode === "light" ? "white" : "grey",
+            color: props.mode === "light" ? "black" : "white",
+          }}
           id="myBox"
           rows={10}
         ></textarea>
@@ -75,38 +78,45 @@ const removeExtraSpaces =()=>{
           Convert text to LowerCase
         </button>
 
-        <button className="btn btn-primary my-3 mx-1" onClick={()=>handleCapClick()}>
+        <button
+          className="btn btn-primary my-3 mx-1"
+          onClick={() => handleCapClick()}
+        >
           Convert text to Title Case
         </button>
 
         <button
           className="btn btn-primary my-3 mx-1"
-          onClick={handleClearClick}>
+          onClick={handleClearClick}
+        >
           Clear your Text
         </button>
 
-        <button className="btn btn-primary my-3 mx-1" onClick={()=>handleCopy()}>
+        <button
+          className="btn btn-primary my-3 mx-1"
+          onClick={() => handleCopy()}
+        >
           Copy your text
-        </button> 
+        </button>
 
-        <button className="btn btn-primary my-3 mx-1" onClick={()=>removeExtraSpaces()}>
-         Remove Extra Spaces
-        </button> 
-        
-
+        <button
+          className="btn btn-primary my-3 mx-1"
+          onClick={() => removeExtraSpaces()}
+        >
+          Remove Extra Spaces
+        </button>
       </div>
 
-      <div className="container mt-0" style={{color : props.mode==='light'?'black':'white'}}>
+      <div
+        className="container mt-0"
+        style={{ color: props.mode === "light" ? "black" : "white" }}
+      >
         <h3>Your text summary</h3>
 
-        {/* <p>
-          {text.split(" ").length} words and {text.length} charcaters
-        </p> */}
-
         <p>
-          {`${numberOfWords} words`} words and {text.length} charcaters
+          {`${numberOfWords} words`} and {text.length} characters.
         </p>
-      
+
         <p>
           Approximately you can read it in {0.008 * text.split(" ").length}{" "}
           Minutes
